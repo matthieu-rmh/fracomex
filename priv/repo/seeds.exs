5865e90e458174f428f3d2f727ed03c2c567de1e
Alain Nambi <alainnambi@gmail.com>
2022-07-18 10:37:45 +0300

    populate database with item, item_family and item_sub_family data

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -31,3 +31,86 @@ Fracomex.Repo.insert!(%City{name: "Dembéni", zipcode: "97660"})
 Fracomex.Repo.insert!(%City{name: "Chiconi", zipcode: "97670"})
 Fracomex.Repo.insert!(%City{name: "Ouangani", zipcode: "97670"})
 Fracomex.Repo.insert!(%City{name: "Tsingoni", zipcode: "97680"})
+
+alias Fracomex.Products.{Item, ItemFamily, ItemSubFamily}
+
+items = [
+  %Item{
+    item_id: "00110",
+    caption: "Legrand - Conduit cintrable ICTA 3522 Rai'Gliss Ø110mm  -  noir à bandes grises",
+    sale_price_vat_excluded: 21.72000000,
+    item_image: "/images/slider1.png",
+    image_version: 0,
+    real_stock: 0.00,
+    stock_status: false,
+    item_family_id: "",
+    item_sub_family_id: ""
+  },
+  %Item{
+    item_id: "00005",
+    caption: "Legrand - Goulotte gaine de protection GP 100 GS dimensions 100x73x135mm",
+    sale_price_vat_excluded: 54.37000000,
+    item_image: "/images/slider2.png",
+    image_version: 0,
+    real_stock: 0.00,
+    stock_status: false,
+    item_family_id: "",
+    item_sub_family_id: ""
+  },
+  %Item{
+    item_id: "510136",
+    caption: "PINCEAU ROND 28MM",
+    sale_price_vat_excluded: 5.00000000,
+    item_image: "/images/slider3.png",
+    image_version: 0,
+    real_stock: 0.00,
+    stock_status: false,
+    item_family_id: "",
+    item_sub_family_id: ""
+  }
+]
+
+items_families = [
+  %ItemFamily{
+    item_family_id: "",
+    caption: "Gaine de protection"
+  },
+  %ItemFamily{
+    item_family_id: "",
+    caption: "Conduits"
+  },
+  %ItemFamily{
+    item_family_id: "",
+    caption: "Pinceaux"
+  }
+]
+
+items_sub_families = [
+  %ItemSubFamily{
+    item_sub_family_id: "",
+    caption: "Gaine de protection GP",
+    item_family_id: ""
+  },
+  %ItemSubFamily{
+    item_sub_family_id: "",
+    caption: "Conduit cintrable",
+    item_family_id: ""
+  },
+  %ItemSubFamily{
+    item_sub_family_id: "",
+    caption: "Pinceaux ronds",
+    item_family_id: ""
+  }
+]
+
+for item <- items do
+  Fracomex.Repo.insert!(item)
+end
+
+for item_family <- items_families do
+  Fracomex.Repo.insert!(item_family)
+end
+
+for item_sub_family <- items_sub_families do
+  Fracomex.Repo.insert!(item_sub_family)
+end
