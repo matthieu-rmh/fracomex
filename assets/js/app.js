eb266da0eae14a253e8f28d107a40849bac5c736
Alain Nambi <alainnambi@gmail.com>
2022-07-20 07:49:50 +0300

    topbar: in liveview, show topbar if only page take more than 200ms to load

--- assets/js/app.js
+++ assets/js/app.js
@@ -755,9 +755,26 @@ let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("
 let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}, hooks: Hooks})
 
 // Show progress bar on live navigation and form submits
+// topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
+// window.addEventListener("phx:page-loading-start", info => topbar.show())
+// window.addEventListener("phx:page-loading-stop", info => topbar.hide())
+
+
+// Show progress bar on live navigation and form submits. Only displays if still
+// loading after 200msec
 topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
-window.addEventListener("phx:page-loading-start", info => topbar.show())
-window.addEventListener("phx:page-loading-stop", info => topbar.hide())
+
+let topBarScheduled = undefined;
+window.addEventListener("phx:page-loading-start", () => {
+  if(!topBarScheduled) {
+    topBarScheduled = setTimeout(() => topbar.show(), 200);
+  };
+});
+window.addEventListener("phx:page-loading-stop", () => {
+  clearTimeout(topBarScheduled);
+  topBarScheduled = undefined;
+  topbar.hide();
+});
 
 // connect if there are any LiveViews on the page
 liveSocket.connect()
