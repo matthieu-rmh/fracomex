d466b8b81135c76c1fcfda7a8861330cf74e4ce0
alain <alainnambi@gmail.com>
2022-07-25 20:08:15 +0300

    remove unused code

--- lib/fracomex_web/live/product_live.ex
+++ lib/fracomex_web/live/product_live.ex
@@ -57,39 +57,6 @@ defmodule FracomexWeb.Live.ProductLive do
      |> push_redirect(to: Routes.product_path(socket, :product_details, item_caption, item_id))}
   end
 
-  # def handle_event("sub", params, socket) do
-  #   list_panier = socket.assigns.list_panier
-  #   list_quantite = socket.assigns.list_quantite
-  #   index = Enum.find_index(list_panier, fn x -> x == params["idp"] end)
-  #   quantite = minus(Enum.fetch!(list_quantite, index))
-  #   list_quantite = List.update_at(list_quantite, index, &(&1 - &1 + quantite))
-  #   somme = somme_prix(list_quantite, list_panier, socket.assigns.produits)
-  #   {:noreply, socket |> assign(somme: somme, list_quantite: list_quantite)}
-  # end
-
-  # def handle_event("add", params, socket) do
-  #   list_panier = socket.assigns.list_panier
-  #   list_quantite = socket.assigns.list_quantite
-  #   produit = ProdRequette.get_produit_by_id_produit(params["idp"])
-  #   max = produit.stockmax |> Decimal.to_integer
-  #   index = Enum.find_index(list_panier, fn x -> x == params["idp"] end)
-  #   quantite = maxus(Enum.fetch!(list_quantite, index), max)
-  #   list_quantite = List.update_at(list_quantite, index, &(&1 - &1 + quantite))
-  #   somme = somme_prix(list_quantite, list_panier, socket.assigns.produits)
-  #   {:noreply, socket |> assign(somme: somme, list_quantite: list_quantite)}
-  # end
-
-  # def handle_event("sup", params, socket) do
-  #   list_panier = socket.assigns.list_panier
-  #   list_quantite = socket.assigns.list_quantite
-  #   index = Enum.find_index(list_panier, fn x -> x == params["idp"] end)
-  #   list_panier = List.delete_at(list_panier, index)
-  #   list_quantite = List.delete_at(list_quantite, index)
-  #   produits = list_panier |> produits_
-  #   somme = somme_prix(list_quantite, list_panier, produits)
-  #   {:noreply, socket |> assign(somme: somme, list_quantite: list_quantite, list_panier: list_panier, produits: produits) |> put_flash(:error, "Produit effacé !!")}
-  # end
-
   def handle_event("add-product-to-cart", params, socket) do
     item_id = params["item_id"]
     item = Products.get_item!(item_id)
