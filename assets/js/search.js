fc2d9d06215b838edd5ddb649c3b58cbf4e10a4f
ljn <fanantenaandria@gmail.com>
2022-08-02 11:25:16 +0200

    changed style css

--- assets/js/search.js
+++ assets/js/search.js
@@ -3,17 +3,15 @@ $( document ).ready(function() {
 				$("#search-nav").slideToggle("500", "easeInOutCirc");
 				$(".sb").focus();
 				$("#search").hide("500", "easeInOutCirc");
-				$("#header-carousel").css( { "margin-top" : "120px","transition" : "0.3s" } );
-				$(".top-section").css( { "margin-top" : "170px","transition" : "0.3s" } );
-				
+				$("#header-carousel").css( { "margin-top" : "130px","transition" : "0.3s" } );
+				$(".top-section").css( { "margin-top" : "140px","transition" : "0.3s" } );
 				});
 				$("#close-search").click(function(){
 				$("#search-nav").slideToggle("500", "easeInOutCirc");
 				$(".sb").focus();
 				$("#search").show("500", "easeInOutCirc");
 				$("#header-carousel").css( { "margin-top" : "80px","transition" : "0.3s" } );
-				$(".top-section").css( { "margin-top" : "110px", "transition" : "0.3s" } );
-				
+				$(".top-section").css( { "margin-top" : "90px", "transition" : "0.3s" } );
 				});
 });
 				
